28a385d3311c1bdcfad3d12447302f1a1da75f13
Samarth Gupta <livesamarthgupta@gmail.com>
2020-12-07 19:36:47 +0530

    Update About.js
    Updated about

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,10 +15,11 @@ const About = () => {
               </a>
               <p className="float-right text-primary">2019 - Present</p>
               <ul className="ui bulleted list">
-                <li className="item">Presently working in an agile environment for the design, development, delivery and support of IBM-Tririga Platform.</li>
-                <li className="item">Rebuilding the frontend of IBM-Tririga from JSPs to ReactJS which decreased page load times to below 2 seconds and improved developer iteration speed dramatically.</li>
-                <li className="item">Was responsible for a11y for the whole application and got it to a score of 93 in the lighthouse report.</li>
-                <li className="item">Also, implemented various Restful APIs in Java/Spring that helped in rebuilding the UI using ReactJS.</li>
+                <li className="item">Presently working in an agile environment for the design, development, delivery and support of IBM-TRIRIGA Platform.</li>
+                <li className="item">IBM-TRIRIGA is used by Fortune-500 companies such as Google, Apple, Adobe and more as Integrated Workplace Management System.</li>
+                <li className="item">Full-Stack Developer - Wrote and reviewed code for JS using React/Redux. Also, implemented various Rest APIs in Java/Spring that was consumed by UI.</li>
+                <li className="item">Worked with Report Renderer Team, in React/Redux to rebuild the Report Renderer from older JSP based engine to UX-Next Renderer.</li>
+                <li className="item">Was responsible for a11y for the whole application and got it to a score of 93 in the Lighthouse report.</li>
                 <li className="item">Fixed various defects in the platform code to make it more stable.</li>
               </ul>
             </li>
