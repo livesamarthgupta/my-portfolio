1549dd690bf409309ee1f0eca38bffecd5eebc05
Samarth Gupta <livesamarthgupta@gmail.com>
2021-04-01 21:50:14 +0530

    Added comments when leaving wipro

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,10 +15,11 @@ const About = () => {
               </a>
               <p className="float-right text-primary">2019 - Present</p>
               <ul className="ui bulleted list">
-                <li className="item">Presently working in an agile environment for the design, development, delivery and support of IBM-TRIRIGA Platform.</li>
-                <li className="item">IBM-TRIRIGA is used by Fortune-500 companies such as Google, Apple, Adobe and more as Integrated Workplace Management System.</li>
-                <li className="item">Full-Stack Developer - Wrote and reviewed code for JS using React/Redux. Also, implemented various Rest APIs in Java/Spring that was consumed by UI.</li>
-                <li className="item">Worked with Report Renderer Team, in React/Redux to rebuild the Report Renderer from older JSP based engine to UX-Next Renderer.</li>
+                <li className="item">Worked in an agile environment for the design, development, delivery and support of IBM-TRIRIGA Platform. IBM-TRIRIGA is used by Fortune-500 companies such as Google, Apple, Adobe and more as Integrated Workplace Management System.</li>
+                <li className="item">Full-Stack Developer: Wrote and Reviewed code in JavaScript/React/Redux. Also, implemented various Rest APIs in Java/Spring that was consumed by the UI.</li>
+                <li className="item">Worked with UX-Next Team to build the UX-Next Report Renderer from scratch.</li>
+                <li className="item">RFEs: Implemented a new filter operator(”In Range”) for the back-end filter engine, Implemented XLS export for BIRT Reports in custom workflows.</li>
+                <li className="item">Implemented a new web component with features such as session expiration warning and session renewal for SSO/Non-SSO IBM-TRIRIGA environments.</li>
                 <li className="item">Was responsible for a11y for the whole application and got it to a score of 93 in the Lighthouse report.</li>
                 <li className="item">Fixed various defects in the platform code to make it more stable.</li>
               </ul>
